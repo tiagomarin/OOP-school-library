16e5c7838a0e2adf02cbedd1f036c5b1183c9c2c
Tiago Marin <tiago.lelinski@gmail.com>
2022-10-12 18:17:41 -0300

    create class books rentals and classroom & add associations to them

--- person.rb
+++ person.rb
@@ -1,18 +1,31 @@
 require 'securerandom'
-
-class Person
+require_relative './nameable'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
+require_relative './rental'
+class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = SecureRandom.uuid
+    @rentals = []
     @name = name
     @age = age
-    @parent_permission = parent_permission
+    @parent_permission = !parent_permission.nil?
   end
 
-  def can_use_services?(parent_permission)
-    of_age || parent_permission ? true : false
+  def add_rental(book)
+    Rental.new(book, self)
+  end
+
+  def can_use_services?
+    of_age || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
@@ -22,5 +35,9 @@ class Person
   end
 end
 
-person = Person.new(12, 'John', parent_permission: false)
-puts person.name
+person = Person.new(22, 'maximilianus')
+pp person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+pp capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+pp capitalized_trimmed_person.correct_name

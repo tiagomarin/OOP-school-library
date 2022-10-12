f22033e4d097f1be724ba2e81e1c0cf69995d740
Tiago Marin <tiago.lelinski@gmail.com>
2022-10-12 18:14:51 -0300

    create class books rentals and classroom & add associations to them

--- person.rb
+++ person.rb
@@ -1,25 +1,31 @@
 require 'securerandom'
 require_relative './nameable'
-require './capitalize_decorator'
-require './trimmer_decorator'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
+require_relative './rental'
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = SecureRandom.uuid
+    @rentals = []
     @name = name
     @age = age
     @parent_permission = !parent_permission.nil?
   end
 
-  def correct_name
-    @name
+  def add_rental(book)
+    Rental.new(book, self)
   end
 
   def can_use_services?
     of_age || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   private
--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(classroom, age, name, parent_permission)
     super(age, name, parent_permission)
@@ -10,5 +10,10 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
